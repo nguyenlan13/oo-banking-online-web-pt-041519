bd57f3e7176e81b2d8d8547f752a34ece92bbd95
Lan Nguyen <nguyenlan13@gmail.com>
2019-05-27 00:39:39 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -28,7 +28,7 @@ class Transfer
   end
 
   def reverse_transfer
-    if valid?
+    if valid? && status = "complete"
       @receiver.balance -= @amount
       @sender.balance += @amount
       @status = "reversed"
